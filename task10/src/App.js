3fec83c04500d80236f744901bf6bd29a248d7d4
gulcannat <juliaparadise0801@gmail.com>
2022-12-16 12:22:48 +0400

    first commit

---
+++ task10/src/App.js
@@ -0,0 +1,46 @@
+import React from "react";
+import List from "./List";
+
+export default class App extends React.Component {
+  state = {
+    user: []
+  };
+
+  getData = () => {
+    fetch("https://5ea5ca472d86f00016b4626d.mockapi.io/brotherhood")
+      .then((resp) => resp.json())
+      .then((data) => {
+        this.setState({ user: data });
+      });
+  };
+  componentDidMount() {
+    this.getData();
+  }
+
+  searchList = () => {
+    const inputText = document.querySelector(".inpt").value;
+    const copyUsers = this.state.user;
+    const userFinding = copyUsers.filter((item) => item.name == inputText);
+    this.setState({ user: userFinding });
+    console.log(userFinding);
+  };
+
+  render() {
+    return (
+      <div className="Main-app">
+        <div className="App">
+          {this.state.user.map((item) => {
+            return <List {...item} />;
+          })}
+        </div>
+        <input className="inpt" type={"text"}></input>
+        <button type="button" onClick={this.searchList}>
+          {" "}
+          Axtarish et
+        </button>
+        <div className="exportDiv"></div>
+      </div>
+    );
+  }
+}
+
